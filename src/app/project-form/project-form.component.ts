5f321da7f3c0ba424ea1f32fc61bc5a0be78c89d
SnakeDoctor <23417772+SnakeDocor@users.noreply.github.com>
2020-04-22 12:08:31 +0200

    working project changes

--- src/app/project-form/project-form.component.ts
+++ src/app/project-form/project-form.component.ts
@@ -1,15 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import{ProjectService} from '../project.service';
+import {Project} from '../project';
+import {ProjectListComponent} from '../project-list/project-list.component';
 
 @Component({
   selector: 'app-project-form',
   templateUrl: './project-form.component.html',
-  styleUrls: ['./project-form.component.css']
+  styleUrls: ['./project-form.component.css'],
+  providers: [ProjectService]
 })
 export class ProjectFormComponent implements OnInit {
 
-  constructor() { }
 
-  ngOnInit(): void {
+  @Input()
+  projectList: ProjectListComponent;
+
+  project = new Project();
+  
+  constructor(private projectService: ProjectService) { }
+
+  ngOnInit(){}
+
+  public save() {
+    this.projectService.save(this.project).subscribe(
+      () => this.projectList.reloadAll()
+    );
   }
+
+  clear(){
+    this.project.projectName = '';
+    this.project.deadline = null;
+    }
+
 
 }
--- src/app/project.service.ts
+++ src/app/project.service.ts
@@ -1,9 +1,27 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {Project} from './project';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProjectService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+
+  findAll(): Observable<Project[]>  {
+    return this.http.get<any>('http://localhost:8080/project')
+  }
+
+  save(project: Project) {
+    return this.http.post('http://localhost:8080/project', project)
+  }
+
+  delete(id) {
+    return this.http.delete('http://localhost:8080/project/' + id)
+  }
+
+
 }
