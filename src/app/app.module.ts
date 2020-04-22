69f3511968020c050dbf9cb758e2e03af6dd1ae0
SnakeDoctor <23417772+SnakeDocor@users.noreply.github.com>
2020-04-22 10:04:08 +0200

    project function added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProjectListComponent } from './project-list/project-list.component';
+import { ProjectFormComponent } from './project-form/project-form.component';
+import { ProjectComponent } from './project/project.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProjectListComponent,
+    ProjectFormComponent,
+    ProjectComponent
   ],
   imports: [
     BrowserModule,
