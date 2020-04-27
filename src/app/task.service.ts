23099769ac0d08559c884e59da19c65247bff25e
REP <inge@van-haaften.nl>
2020-04-27 13:33:35 +0200

    Edit button functionality for task and project

--- src/app/task.service.ts
+++ src/app/task.service.ts
@@ -8,19 +8,24 @@ import {Project} from './project';
   providedIn: 'root'
 })
 export class TaskService {
-
+  
   constructor(private http: HttpClient) { }
-
+  
   findAll(): Observable<Task[]>  {
     return this.http.get<any>('http://localhost:8080/task')
   }
-
+  
   save(task: Task) {
     return this.http.post('http://localhost:8080/task', task)
   }
-
+  
   delete(id) {
     return this.http.delete('http://localhost:8080/task/' + id)
   }
-
+  
+  //to edit the listing.
+  patchTask(id:number, task:Task){
+    return this.http.patch('http://localhost:8080/task/' + id, task)
+  }
+  
 }
