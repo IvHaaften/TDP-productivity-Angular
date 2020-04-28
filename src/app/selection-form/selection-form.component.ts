837f37f9acdf94b935fd3488933193dec9f51f15
SnakeDoctor <23417772+SnakeDocor@users.noreply.github.com>
2020-04-28 12:27:40 +0200

    crap

--- src/app/selection-form/selection-form.component.ts
+++ src/app/selection-form/selection-form.component.ts
@@ -2,30 +2,28 @@ import { Component, OnInit,Input } from '@angular/core';
 import{Selection} from '../selection';
 import {SelectionListComponent} from '../selection-list/selection-list.component';
 import { SelectionService } from '../selection.service';
+import {Task} from '../task'
+import { TaskService } from '../task.service';
 
 @Component({
   selector: 'app-selection-form',
   templateUrl: './selection-form.component.html',
   styleUrls: ['./selection-form.component.css'],
-  providers: [SelectionService]
+  providers: [TaskService]
 })
 export class SelectionFormComponent implements OnInit {
 
   selection = new Selection();
+  tasks: Task[];
 
   // constructor(private selectionService: SelectionService) { }
 
   ngOnInit(): void {
   }
 
-  // public save() {
-   
-  // }
-
   clear(){
     this.selection.projectNumber = null;
     this.selection.timeWindow = null;
     
     }
-
 }
