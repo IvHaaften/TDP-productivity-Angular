fff4f155a540828a60b295f9046b06c0626d7818
Inge van Haaften <63044073+IvHaaften@users.noreply.github.com>
2020-04-28 09:31:34 +0200

    Pruned app.module.ts
    Removed redundant imports, outdated commented lines and lines that refferred to removed components.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,70 +11,10 @@ import { TaskFormComponent } from './task-form/task-form.component';
 import { ProjectListComponent, ProjectEditModal } from './project-list/project-list.component';
 import { ProjectFormComponent } from './project-form/project-form.component';
 
-import { ProjectEditComponent } from './project-edit/project-edit.component';
-import { TaskEditComponent } from './task-edit/task-edit.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-//import { MatInputModule } from '@angular/material/input';
-
-
 import {MaterialDesignModule} from './material-design/material-design.module';
 import { SelectionFormComponent } from './selection-form/selection-form.component';
 import { SelectionListComponent } from './selection-list/selection-list.component';
-/*
-
-//  datepicker
-import {NgModule} from '@angular/core';
-import {A11yModule} from '@angular/cdk/a11y';
-import {ClipboardModule} from '@angular/cdk/clipboard';
-import {DragDropModule} from '@angular/cdk/drag-drop';
-import {PortalModule} from '@angular/cdk/portal';
-import {ScrollingModule} from '@angular/cdk/scrolling';
-import {CdkStepperModule} from '@angular/cdk/stepper';
-import {CdkTableModule} from '@angular/cdk/table';
-import {CdkTreeModule} from '@angular/cdk/tree';
-import {MatAutocompleteModule} from '@angular/material/autocomplete';
-import {MatBadgeModule} from '@angular/material/badge';
-import {MatBottomSheetModule} from '@angular/material/bottom-sheet';
-import {MatButtonModule} from '@angular/material/button';
-import {MatButtonToggleModule} from '@angular/material/button-toggle';
-import {MatCardModule} from '@angular/material/card';
-import {MatCheckboxModule} from '@angular/material/checkbox';
-import {MatChipsModule} from '@angular/material/chips';
-import {MatStepperModule} from '@angular/material/stepper';
-import {MatDatepickerModule} from '@angular/material/datepicker';
-import {MatDialogModule} from '@angular/material/dialog';
-import {MatDividerModule} from '@angular/material/divider';
-import {MatExpansionModule} from '@angular/material/expansion';
-import {MatGridListModule} from '@angular/material/grid-list';
-import {MatIconModule} from '@angular/material/icon';
-import {MatInputModule} from '@angular/material/input';
-import {MatListModule} from '@angular/material/list';
-import {MatMenuModule} from '@angular/material/menu';
-import {MatNativeDateModule, MatRippleModule} from '@angular/material/core';
-import {MatPaginatorModule} from '@angular/material/paginator';
-import {MatProgressBarModule} from '@angular/material/progress-bar';
-import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
-import {MatRadioModule} from '@angular/material/radio';
-import {MatSelectModule} from '@angular/material/select';
-import {MatSidenavModule} from '@angular/material/sidenav';
-import {MatSliderModule} from '@angular/material/slider';
-import {MatSlideToggleModule} from '@angular/material/slide-toggle';
-import {MatSnackBarModule} from '@angular/material/snack-bar';
-import {MatSortModule} from '@angular/material/sort';
-import {MatTableModule} from '@angular/material/table';
-import {MatTabsModule} from '@angular/material/tabs';
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatTooltipModule} from '@angular/material/tooltip';
-import {MatTreeModule} from '@angular/material/tree';
-
-// end date
-*/
-
-=======
-
-
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MaterialDesignModule} from './material-design/material-design.module';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 
@@ -85,17 +25,10 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
     TaskFormComponent,
     TaskEditModal,
     ProjectListComponent,
-    ProjectFormComponent,
-
-    ProjectEditComponent,
-    TaskEditComponent,
+    ProjectFormComponent,    
+    ProjectEditModal,
     SelectionFormComponent,
-    SelectionListComponent,
-=======
-
-    ProjectEditModal
-
-
+    SelectionListComponent
   ],
   imports: [
     BrowserModule,
