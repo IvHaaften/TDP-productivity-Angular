2febba92ead13c1eb6496a5d30017fd95c1769b0
EdwinBolt <edwinbolt@live.nl>
2020-05-11 11:26:50 +0200

    first step towards hosting

--- src/app/user.service.ts
+++ src/app/user.service.ts
@@ -11,25 +11,25 @@ export class UserService {
   userId:number;
   
   login(user: User){
-    return this.http.get<number>('http://localhost:8080/user/login?user=' + user.username + '&password=' + user.password)
+    return this.http.get<number>('/user/login?user=' + user.username + '&password=' + user.password)
   }
   
   constructor(private http: HttpClient) { }
   
   findAll(): Observable<User[]>  {
-    return this.http.get<any>('http://localhost:8080/user')
+    return this.http.get<any>('/user')
   }
   
   save(user: User) {
-    return this.http.post('http://localhost:8080/user', user)
+    return this.http.post('/user', user)
   }
   
   delete(id) {
-    return this.http.delete('http://localhost:8080/user/' + id)
+    return this.http.delete('/user/' + id)
   }
   
   //to edit the listing.
   patchUser(id:number, user:User){
-    return this.http.patch('http://localhost:8080/user/' + id, user)
+    return this.http.patch('/user/' + id, user)
   }
 }
